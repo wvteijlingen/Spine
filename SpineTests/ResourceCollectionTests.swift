18f850b1975cf7a82660413cbf1c9cff69bb58ef
Ward van Teijlingen <w.van.teijlingen@gmail.com>
2016-10-02 15:02:49 +0200

    Updated test target for macOS.

--- SpineTests/ResourceCollectionTests.swift
+++ SpineTests/ResourceCollectionTests.swift
@@ -6,7 +6,6 @@
 //  Copyright (c) 2015 Ward van Teijlingen. All rights reserved.
 //
 
-import UIKit
 import XCTest
 
 class ResourceCollectionTests: XCTestCase {
--- SpineTests/SpineTests.swift
+++ SpineTests/SpineTests.swift
@@ -6,7 +6,6 @@
 //  Copyright (c) 2014 Ward van Teijlingen. All rights reserved.
 //
 
-import UIKit
 import XCTest
 import SwiftyJSON
 import BrightFutures
